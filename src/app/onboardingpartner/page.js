40509f83f6daea5d980540fdee66208bc6cbd3a4
kiran <kiranugale239@gmail.com>
2025-03-27 10:07:33 +0530

    add onboarding_partner page

---
+++ src/app/onboardingpartner/page.js
@@ -0,0 +1,11 @@
+import OnboardingHeroSection from "@/Component/onboarding_partner/HeroSection";
+import OnboardingMarketReality from "@/Component/onboarding_partner/OnboardingMarket";
+
+export default function OnboardingPage() {
+  return (
+    <div className="w-full m-0 p-0 ">
+      <OnboardingHeroSection />
+      <OnboardingMarketReality />
+    </div>
+  );
+}
